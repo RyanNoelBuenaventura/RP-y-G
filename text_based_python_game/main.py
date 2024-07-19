f73b8efbd11e46343f8765044b96da20a8abe42a
RyanNoelBuenaventura <140692281+RyanNoelBuenaventura@users.noreply.github.com>
2024-07-19 14:15:11 -0700

    Add files via upload

---
+++ text_based_python_game/main.py
@@ -0,0 +1,50 @@
+import random
+
+class character:
+    def __init__(self, health, stamina, mana):
+        self.health = health
+        self.stamina = stamina
+        self.mana = mana
+
+def player_attack():
+    asdsa
+
+
+def random_orc():
+    print("hello human")
+
+    print("ATTACK")
+    player.health = player.health - random.randint(0,120)
+
+    print(f"{player.health}")
+
+
+    death_check()
+
+
+def death_check():
+    if player.health <= 0:
+        print("Game Over")
+        exit()
+
+
+
+player = character(100, 75, 50)
+
+print(f"{player.health}")
+
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+random_orc()
+
+
+
+
